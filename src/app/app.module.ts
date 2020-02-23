f201c524c21811c39070eacd3c1503d6f598b65d
LeonAdeoye <leon.adeoye@gmail.com>
2020-02-23 13:50:11 +0800

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainGridComponent } from './components/main-grid/main-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainGridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

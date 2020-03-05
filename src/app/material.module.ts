8eb9f87385b0139385025b2f4998e0200b737dc9
LeonAdeoye <leon.adeoye@gmail.com>
2020-03-06 00:23:29 +0800

    angular material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,22 @@
 import {NgModule} from '@angular/core';
-
-import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
 
-@NgModule(
-{
-  imports:
-  [
+
+@NgModule({
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
     MatIconModule,
-    MatInputModule
+    MatListModule,
   ],
-  exports:
-  [
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
   ]
 })
-
 export class MaterialModule {}

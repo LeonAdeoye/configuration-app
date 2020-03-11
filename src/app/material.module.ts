f8257f1b91d1b36cf897e5c0092e9bdc608b7ab4
LeonAdeoye <leon.adeoye@gmail.com>
2020-03-11 22:25:49 +0800

    material form UI

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,22 +1,15 @@
-import {NgModule} from '@angular/core';
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { NgModule } from '@angular/core';
 import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
-
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   imports: [
-    MatSidenavModule,
-    MatToolbarModule,
     MatIconModule,
-    MatListModule,
+    MatInputModule
   ],
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
     MatIconModule,
-    MatListModule,
+    MatInputModule
   ]
 })
 export class MaterialModule {}

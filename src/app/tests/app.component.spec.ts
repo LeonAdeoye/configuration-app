86064aaf469f4be03daa97f37aea9d4ba25028f5
LeonAdeoye <leon.adeoye@gmail.com>
2020-03-24 19:20:59 +0800

    started unit testing

--- src/app/tests/app.component.spec.ts
+++ src/app/tests/app.component.spec.ts
@@ -1,28 +1,50 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from '../app.component';
+import { ConfigurationService } from "../services/configuration.service";
+import { BootstrapService } from "../services/bootstrap.service";
+import { LoggingService } from "../services/logging.service";
+import { HttpClientModule } from "@angular/common/http";
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
+describe('AppComponent', () =>
+{
+  beforeEach(async(() =>
+  {
+    const spyConfigurationService = jasmine.createSpyObj('ConfigurationService', ['loadAllConfigurations']);
+    const spyBootstrapService = jasmine.createSpyObj('BootstrapService', ['launch']);
+    const spyLoggingService = jasmine.createSpyObj('LoggingService', ['log']);
+
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        HttpClientModule
+      ],
+      providers: [
+        { provide: ConfigurationService, useClass: spyConfigurationService },
+        { provide: BootstrapService, useClass: spyBootstrapService },
+        { provide: LoggingService, useClass: spyLoggingService }
+      ]
+
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should create the app', () =>
+  {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'configuration-app'`, () => {
+  it(`should have as title 'configuration-app'`, () =>
+  {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('configuration-app');
   });
 
-  it('should render title', () => {
+  it('should render title', () =>
+  {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
--- src/app/tests/bootstrap.service.spec.ts
+++ src/app/tests/bootstrap.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BootstrapService } from '../services/bootstrap.service';
+import { AppComponent } from "../app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ConfigurationService } from "../services/configuration.service";
+import { LoggingService } from "../services/logging.service";
 
-describe('BootstrapService', () => {
+describe('BootstrapService', () =>
+{
   let service: BootstrapService;
+  const spyConfigurationService = jasmine.createSpyObj('ConfigurationService', ['loadAllConfigurations']);
+  const spyLoggingService = jasmine.createSpyObj('LoggingService', ['log']);
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(() =>
+  {
+    TestBed.configureTestingModule({
+      declarations:
+      [
+      ],
+      imports:
+      [
+      ],
+      providers:
+      [
+        { provide: ConfigurationService, useClass: spyConfigurationService },
+        { provide: LoggingService, useClass: spyLoggingService }
+      ]
+
+    }).compileComponents();
+
     service = TestBed.inject(BootstrapService);
   });
 
-  it('should be created', () => {
+  it('should be created', () =>
+  {
     expect(service).toBeTruthy();
   });
 });

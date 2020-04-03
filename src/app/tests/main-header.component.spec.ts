231973e33220969ceff84f17ea5b66a4a391fb4a
LeonAdeoye <leon.adeoye@gmail.com>
2020-04-03 20:36:42 +0800

    main-header component unit testing

--- src/app/tests/main-header.component.spec.ts
+++ src/app/tests/main-header.component.spec.ts
@@ -1,16 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { MainHeaderComponent } from '../components/main-header/main-header.component';
 import { ConfigurationService } from "../services/configuration.service";
 import { LoggingService } from "../services/logging.service";
 import { GridSearchService } from "../services/grid-search.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ConfigurationServiceMock } from "./mock-configuration.service";
 
 describe('MainHeaderComponent', () =>
 {
   let component: MainHeaderComponent;
   let fixture: ComponentFixture<MainHeaderComponent>;
-  const spyConfigurationService = jasmine.createSpyObj('ConfigurationService', ['loadAllConfigurations', 'getAllConfigurations','setCurrentUser', 'getCurrentUser']);
+
   const spyLoggingService = jasmine.createSpyObj('LoggingService', ['log', 'initialize']);
   const spyGridSearchService = jasmine.createSpyObj('GridSearchService', ['setText']);
 
@@ -27,7 +28,7 @@ describe('MainHeaderComponent', () =>
       ],
       providers:
       [
-        { provide: ConfigurationService, useValue: spyConfigurationService },
+        { provide: ConfigurationService, useClass: ConfigurationServiceMock },
         { provide: LoggingService, useValue: spyLoggingService },
         { provide: GridSearchService, useValue: spyGridSearchService }
       ]
@@ -45,5 +46,18 @@ describe('MainHeaderComponent', () =>
   it('should create', () =>
   {
     expect(component).toBeTruthy();
+  });
+
+  describe('addConfiguration', () =>
+  {
+    it('should call configuration service addConfigurationSubject next', inject([ConfigurationService], (configurationService) =>
+    {
+      // Arrange
+      spyOn(configurationService.addConfigurationSubject, 'next');
+      // Act
+      component.addConfiguration();
+      // Assert
+      expect(configurationService.addConfigurationSubject.next).toHaveBeenCalled();
+    }));
   });
 });

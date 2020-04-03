d31230ff89833caa0a8fa63d87090a8884741583
LeonAdeoye <leon.adeoye@gmail.com>
2020-04-03 21:14:22 +0800

    more detail component unit testing

--- src/app/tests/detail.component.spec.ts
+++ src/app/tests/detail.component.spec.ts
@@ -4,6 +4,7 @@ import { ConfigurationService } from "../services/configuration.service";
 import { LoggingService } from "../services/logging.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ConfigurationServiceMock } from "./mock-configuration.service";
+import { Configuration } from "../models/configuration";
 
 describe('DetailComponent', () =>
 {
@@ -44,10 +45,101 @@ describe('DetailComponent', () =>
 
   describe('canClear', () =>
   {
-    it('should return false when input configuration is not set', () =>
+    it('should return false when @Input configuration is not set', () =>
     {
       // Act
       let result = component.canClear();
+      // Assert
+      expect(result).not.toBeTruthy();
+    });
+
+    it('should return false when owner, key, and value of @Input configuration are not set', () =>
+    {
+      // Arrange
+      component.configuration = new Configuration(null, null, null, "Papa", "now", "20121223");
+      // Act
+      let result = component.canClear();
+      // Assert
+      expect(result).not.toBeTruthy();
+    });
+
+    it('should return true when only value of @Input configuration is set', () =>
+    {
+      // Arrange
+      component.configuration = new Configuration("Horatio", "Surname", null, "Papa", "now", "20121223");
+      // Act
+      let result = component.canClear();
+      // Assert
+      expect(result).toBeTruthy();
+    });
+
+    it('should return false when only key of @Input configuration is set', () =>
+    {
+      // Arrange
+      component.configuration = new Configuration(null, "Surname", null, "Papa", "now", "20121223");
+      // Act
+      let result = component.canClear();
+      // Assert
+      expect(result).toBeTruthy();
+    });
+
+    it('should return false when only owner of @Input configuration is set', () =>
+    {
+      // Arrange
+      component.configuration = new Configuration("Harper", null, null, "Papa", "now", "20121223");
+      // Act
+      let result = component.canClear();
+      // Assert
+      expect(result).toBeTruthy();
+    });
+  });
+
+  describe('canSave', () =>
+  {
+    it('should return false when @input configuration is not set', () =>
+    {
+      // Act
+      let result = component.canSave();
+      // Assert
+      expect(result).not.toBeTruthy();
+    });
+
+    it('should return true when owner, key and value of @Input configuration are set', () =>
+    {
+      // Arrange
+      component.configuration = new Configuration("Horatio", "Surname", "Adeoye", "Papa", "now", "20121223");
+      // Act
+      let result = component.canSave();
+      // Assert
+      expect(result).toBeTruthy();
+    });
+
+    it('should return false when value of @Input configuration is not set', () =>
+    {
+      // Arrange
+      component.configuration = new Configuration("Horatio", "Surname", null, "Papa", "now", "20121223");
+      // Act
+      let result = component.canSave();
+      // Assert
+      expect(result).not.toBeTruthy();
+    });
+
+    it('should return false when key of @Input configuration is not set', () =>
+    {
+      // Arrange
+      component.configuration = new Configuration("Horatio", null, "Adeoye", "Papa", "now", "20121223");
+      // Act
+      let result = component.canSave();
+      // Assert
+      expect(result).not.toBeTruthy();
+    });
+
+    it('should return false when owner of @Input configuration is not set', () =>
+    {
+      // Arrange
+      component.configuration = new Configuration(null, "Surname", "Adeoye", "Papa", "now", "20121223");
+      // Act
+      let result = component.canSave();
       // Assert
       expect(result).not.toBeTruthy();
     });
@@ -64,5 +156,31 @@ describe('DetailComponent', () =>
       // Assert
       expect(configurationService.saveConfiguration).toHaveBeenCalled();
     }));
+  });
+
+  describe('clear', () =>
+  {
+    it('should clear configuration @Input member variable', () =>
+    {
+      // Arrange
+      component.configuration = new Configuration("Harper", "Surname", "Adeoye", "Papa", "now", "20121223");
+      // Act
+      component.clear();
+      // Assert
+      expect(component.configuration).toEqual(new Configuration());
+    });
+  });
+
+  describe('cancel', () =>
+  {
+    it('should emit close panel event', () =>
+    {
+      // Arrange
+      spyOn(component.closePanelEventEmitter, 'emit');
+      // Act
+      component.cancel();
+      // Assert
+      expect(component.closePanelEventEmitter.emit).toHaveBeenCalled();
+    });
   });
 });
